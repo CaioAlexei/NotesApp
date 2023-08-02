99740cb1b5a45d37139166075c64191e04a62aa9
Caio Alexei <caioalexeilimaazevedo@gmail.com>
2023-08-02 13:14:48 -0300

    Starts here

---
+++ resources/views/index.blade.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="/css/app.css">
+    <title>Document</title>
+</head>
+<body>
+
+    <div class="container">
+
+        <div class="d-flex justify-content-center">
+            <h1>My Notes</h1>
+
+        </div>
+
+    </div>
+
+    <div class="container">
+
+        <div class="d-flex justify-content-center mt-4">
+
+            <button class="btn btn-dark btn-sm">Add Note</button>
+
+        </div>
+
+    </div>
+
+    <div class="container mt-4 d-flex gap-3 flex-wrap">
+
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Note Title</h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary">Note Subtitle</h6>
+                <p class="card-text">Programa isso adddddddddddddddddddddddddddd</p>
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+            </div>
+        </div>
+
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Note Title</h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary">Note Subtitle</h6>
+                <p class="card-text">Programa isso ai amigo caiosdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</p>
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+            </div>
+        </div>
+
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Note Title</h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary">Note Subtitle</h6>
+                <p class="card-text">Programa isso ai amigo caiosdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</p>
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+            </div>
+        </div>
+
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Note Title</h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary">Note Subtitle</h6>
+                <p class="card-text">Programa isso ai amigo caiosdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</p>
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+            </div>
+        </div>
+
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">Note Title</h5>
+                <h6 class="card-subtitle mb-2 text-body-secondary">Note Subtitle</h6>
+                <p class="card-text">Programa isso ai amigo caiosdddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd</p>
+                <a href="#" class="card-link">Card link</a>
+                <a href="#" class="card-link">Another link</a>
+            </div>
+        </div>
+
+    </div>
+
+</body>
+</html>
